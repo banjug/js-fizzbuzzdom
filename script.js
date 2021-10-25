59319a91a892842c0e9e03625d6868c953b097d2
Samuele Miotto <miotto.samuele@gmail.com>
2021-10-25 16:03:31 +0200

    working script

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+// for (let i = 0; i < 100; i++) {
+//     if (i % 15 == 0) {
+//         console.log("fizzbuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("buzz");
+//     } else {
+//         console.log(i);
+//     }
+
+// };
 
 
+const innContainer = document.querySelector('.inner-container');
 
-for (let i = 0; i < 100; i++) {
+for (let i=1; i <=100; i++) {
     if (i % 15 == 0) {
-        console.log("fizzbuzz");
+        innContainer.innerHTML += `<div class="box fizzbuzz">fizzbuzz</div>`;
     } else if (i % 3 === 0) {
-        console.log("fizz");
+        innContainer.innerHTML += `<div class="box fizz">fizz</div>`;
     } else if (i % 5 === 0) {
-        console.log("buzz");
+        innContainer.innerHTML += `<div class="box buzz">buzz</div>`;
     } else {
-        console.log(i);
+        innContainer.innerHTML += `<div class="box ">${i}</div>`;
     }
 
-    
-
-
-};
+}
